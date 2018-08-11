38eb1b63a21717a5d08cd18833fb14974c1d9442
borislipsman <borislipsman@boriss-mbp.fios-router.home>
2018-08-11 15:09:43 -0400

    removed method

--- maven-profiles-in-action/profileProject/src/main/java/com/vaunguard/test/FirstInterface.java
+++ maven-profiles-in-action/profileProject/src/main/java/com/vaunguard/test/FirstInterface.java
@@ -1,7 +1,16 @@
 package com.vaunguard.test;
 
+import java.util.List;
+
+
+/**
+ * 08/11/2018
+ * @author borislipsman
+ *
+ */
 public interface FirstInterface {
 	
 	public void createBrowser();
+	public List<String> getCurrentBrowserProperties();
 
 }
